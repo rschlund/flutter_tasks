0c877be5b6a8a5f2dfabfd26c900511a77388d91
Rainer Schlund <schlund@teco.edu>
2019-01-02 16:39:30 +0100

    Ready

---
+++ lib/aufgabe_6.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'counterPage.dart';
+import 'myListPage.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    // Title
+    title: "Using Tabs",
+    // Home
+    home: new TutorialHome(),
+  ));
+}
+
+class TutorialHome extends StatefulWidget {
+  TutorialHome({Key key}) : super(key: key);
+
+  @override
+  TutorialHomeState createState() => TutorialHomeState();
+}
+
+class TutorialHomeState extends State<TutorialHome> {
+  int _selectedIndex = 0;
+  final List<Widget> _children = [
+    new CounterPage(),
+    new MyList(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: _children[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ac_unit), title: Text('Counter')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.blur_circular), title: Text('Star Wars')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.blueAccent,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
